394b019732637aea034723c4597d050f1ec47e5d
pom <pom@jiminy-dev.medialab>
2010-06-18 12:40:41 +0200

    packaging to pypi

---
+++ gexf/__init__.py
@@ -0,0 +1,4 @@
+from gexf import Gexf
+from gexf import Node
+from gexf import Edge
+from gexf import Graph
---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='pygexf',
+      version='0.1',
+      packages=['gexf'],
+      url='http://github.com/paulgirard/pygexf',
+      author='Paul Girard',
+      author_email='paul.girard@sciences-po.fr'
+      )

39fa1d4238169c151966b2359f602c0a3decd82d
pom <pom@jiminy-dev.medialab>
2012-06-20 19:08:58 +0200

    pygexf now supports 1.2 + lot of refactoring (see comments) + added a new test script

--- test/test.py
+++ test/test.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 
-from gexf import Gexf,GexfImport
+import sys,pprint
+
+sys.path.append('../gexf')
+from _gexf import Gexf, GexfImport
 
 # test helloworld.gexf
 gexf = Gexf("Paul Girard","A hello world! file")
@@ -10,12 +13,32 @@ graph.addNode("0","hello")
 graph.addNode("1","World")
 graph.addEdge("0","0","1")
 
-output_file=open("hellowrld.gexf","w")
+output_file=open("helloworld.gexf","w")
 gexf.write(output_file)
 
 # test GexfImport
-f = open("exp.gexf")
-o = open("exp_bootstrap_bootstrap.gexf", "w+")
+f = open("gexf.net.dynamics_openintervals.gexf")
+gexf_import = Gexf.importXML(f)
+f.close()
+f = open("gexf.net.dynamics_openintervals.gexf")
+gexf_import2 = Gexf.importXML(f)
+f.close()
+print "test gexf comparision "+str(gexf_import==gexf_import2)
 
-gexf_import = GexfImport(f).gexf()
+
+
+graph=gexf_import.graphs[0]
+# display nodes list 
+for node_id,node in graph.nodes.iteritems() : 
+    print node.label
+    pprint.pprint(node.getAttributes(),indent=1,width=1)
+
+# display edges list 
+for edgeid,edge in graph.edges.iteritems() : 
+    print str(graph.nodes[edge.source])+" -> "+str(graph.nodes[edge.target])
+    pprint.pprint(edge.getAttributes(),indent=1,width=1)
+
+
+o = open("gexf.net.dynamics_openintervals_copied.gexf", "w")
+
 gexf_import.write(o)

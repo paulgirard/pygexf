12afdc521973f359b23a0c742d69a92a07250a40
Paul Girard <paul@anomalie.info>
2013-02-12 11:52:47 +0100

    fix #4
    corrected a bug due to refactoring in makeAttributesInstance
     Thanks to MOON-CLJ and panisson

--- gexf/_gexf.py
+++ gexf/_gexf.py
@@ -491,7 +491,7 @@ class Node :
         #self._graph.addDefaultAttributesToNode(self)
         
     def addAttribute(self,id,value,start="",end="",startopen=False,endopen=False) :
-        self._attributes.append(makeAttributeInstance(self,"node",id,value,start,end,startopen,endopen))
+        self._attributes.append(self._graph.attributes.makeAttributeInstance("node",id,value,start,end,startopen,endopen))
             
     def getXML(self) :
         # return lxml etree element
@@ -644,7 +644,7 @@ class Edge :
         
         
     def addAttribute(self,id,value,start="",end="",startopen=False,endopen=False) :
-        self._attributes.append(makeAttributeInstance(self,"node",id,value,start,end,startopen,endopen))
+        self._attributes.append(self._graph.attributes.makeAttributeInstance("edge",id,value,start,end,startopen,endopen))
         
     
     def getXML(self) :

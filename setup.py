763ef97763db66ee92c0d3f4b2ecf384d57fcbb2
amirouche <amirouche.boubekki@gmail.com>
2015-05-29 22:09:47 +0200

    make setup.py executable

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python
 from setuptools import setup
+
 setup(name='pygexf',
       version='0.2.2',
       packages=['gexf'],
       url='http://github.com/paulgirard/pygexf',
       author='Paul Girard',
       author_email='paul.girard@sciences-po.fr'
-      )
+)

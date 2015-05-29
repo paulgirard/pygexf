2a189e61e523704a1853d939aa33eade77532468
amirouche <amirouche.boubekki@gmail.com>
2015-05-29 22:25:55 +0200

    setup.py: add lxml dependency & some classifiers

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python
 from setuptools import setup
 
-setup(name='pygexf',
-      version='0.2.2',
-      packages=['gexf'],
-      url='http://github.com/paulgirard/pygexf',
-      author='Paul Girard',
-      author_email='paul.girard@sciences-po.fr'
+
+setup(
+    name='pygexf',
+    version='0.2.2',
+    packages=['gexf'],
+    url='http://github.com/paulgirard/pygexf',
+    author='Paul Girard',
+    author_email='paul.girard@sciences-po.fr',
+    license='',
+    install_requires=[
+        'lxml',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
 )
